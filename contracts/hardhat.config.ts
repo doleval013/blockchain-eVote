9b0d37669005f0e3052e960ea07c173d96110e8a
ShiranGiladi <shirang16@gmail.com>
2024-08-30 17:44:44 +0300

    with testnet details

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -12,7 +12,7 @@ const config: HardhatUserConfig = {
       url: "http://127.0.0.1:8545" || "",
     },
     goerli: {
-      url: process.env.GOERLI_URL || "",
+      url: process.env.TESTNET_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
