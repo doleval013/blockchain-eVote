cdfc79b7ae1497d93e4de43310561c6c4a6d172d
Dolev Almog <100090773+doleval013@users.noreply.github.com>
2024-08-31 12:57:54 +0300

    Update contractConfigs.js

--- src/configs/contractConfigs.js
+++ src/configs/contractConfigs.js
@@ -1,6 +1,6 @@
 import Evoting_ABI from "@/src/assets/abis/Evoting.json";
 
-const Evoting_Address = process.env.ENV === 'development'
+const Evoting_Address = process.env.environment === 'development'
   ? "0x5FbDB2315678afecb367f032d93F642f64180aa3" // localhost
   : "0xbD3511793B06C051c855c0850A2d7a2De64A7F40"; // sepolia
 
